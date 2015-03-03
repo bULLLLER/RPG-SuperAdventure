fb9e5c986ff314aa8183bda39d4ef5014d19fbbb
Dradee <jmkjaer@hotmail.com>
2015-03-03 23:59:34 +0100

    Test

---
+++ SuperAdventure/Engine/Armor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine {
+    public class Armor : Item {
+        public int ExtraHitPoints { get; set; }
+    }
+}
